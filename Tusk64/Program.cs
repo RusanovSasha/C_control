320f10997e61eed0ebe41133956552a2409359f1
RusanovSasha <107805109+RusanovSasha@users.noreply.github.com>
2022-11-17 19:04:14 +0300

    End codding

---
+++ Tusk64/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задача 64: Задайте значение N. Напишите программу, которая выведет все чётные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+//N = 5 -> "4, 2"
+//N = 8 -> "8, 6, 4, 2,"
+
+Console.Clear();
+Console.Write("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+PrintChisla(n);
+
+// функция вывода чётных чисел
+
+void PrintChisla(int n)
+{
+    if (n > 0)
+    {
+        if (n % 2 == 0)
+        {
+            Console.Write(n);
+            if (n > 2)
+            {
+                Console.Write(", ");
+            }
+        }
+        n--;
+        PrintChisla(n);
+    }
+}
---
+++ Tusk66/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+Console.Clear();
+Console.Write("Введите M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+// делаем возможным введение M = 15; N = 1 и нахождения при этом нужной нам суммы
+
+if (m > n)
+{
+    int i = m;
+    m = n;
+    n = i;
+}
+
+Console.WriteLine($"Сумма натуальных элементов в промежутке между M и N равна {Summa(m, n, 0)}");
+
+// функция нахождения суммы
+
+int Summa(int m, int n, int sum)
+{
+    if (m <= n) { sum = sum + m; m++; sum = Summa(m, n, sum); }
+    return sum;
+}
